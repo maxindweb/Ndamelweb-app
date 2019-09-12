4b148ed7a0af6a7d8a75b59a0e55aae07c26a580
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2019-09-12 10:43:18 +0700

    backend

--- database/migrations/2019_09_12_021933_create_orders_table.php
+++ database/migrations/2019_09_12_021933_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role_id');
-            $table->rememberToken();
+            $table->string('product_id');
+            $table->double('harga_satuan');
+            $table->boolean('is_paid');
+            $table->string('nota_id');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 require('./bootstrap');
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueSwal from 'vue-swal'
+import axios from 'axios';
+import store from './store/store';
